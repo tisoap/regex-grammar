1393c0b843e7b9e694404605e3a6114fd23d1f83
Tiso <tiso@localhost.localdomain>
2015-09-28 16:23:01 -0300

    Removida pasta raiz regex-tradutor \n Biblioteca Junit

---
+++ src/exception/UnrecognisedMetadata.java
@@ -0,0 +1,26 @@
+package exception;
+
+/**
+ * Excecao utilizada quando um metadado "userdata" de
+ * um objeto JSON nao e reconhecido pela aplicacao.
+ */
+public class UnrecognisedMetadata extends MalformedJson {
+
+	private static final long serialVersionUID = 4072490455418228025L;
+
+	public UnrecognisedMetadata() {
+		super();
+	}
+
+	public UnrecognisedMetadata(String message) {
+		super(message);
+	}
+
+	public UnrecognisedMetadata(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public UnrecognisedMetadata(Throwable cause) {
+		super(cause);
+	}
+}
---
+++ src/exception/UnrecognisedRule.java
@@ -0,0 +1,26 @@
+package exception;
+
+/**
+ * Excecao utilizada quando o valor de um metadado do tipo "regra"
+ * nao e reconhecido pela aplicacao.
+ */
+public class UnrecognisedRule extends MalformedJson {
+
+	private static final long serialVersionUID = 8002399404074649275L;
+
+	public UnrecognisedRule() {
+		super();
+	}
+
+	public UnrecognisedRule(String message) {
+		super(message);
+	}
+
+	public UnrecognisedRule(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public UnrecognisedRule(Throwable cause) {
+		super(cause);
+	}
+}
